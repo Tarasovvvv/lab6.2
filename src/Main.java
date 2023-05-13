d307f802d94dc7da9674159ae12d2d8ce96dfcc7
Ivanov Sergey <96431033+qtenebrae@users.noreply.github.com>
2023-05-13 19:17:35 +0400

    broken more
    fix it another one

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,4 @@
-public class Main {
-    public static void main(String[] args) {
-        CelciusTemperature temp1 = new CelciusTemperature(36.6);
-        IKelvin CKA = new CKAdapter(temp1);
-        IFahrenheit CFA = new CFAdapter(temp1);
 
-        FahrenheitTemperature temp2 = new FahrenheitTemperature(36.6);
-        ICelcius FCA = new FCAdapter(temp2);
-        IKelvin FKA = new FKAdapter(temp2);
-
-        KelvinTemperature temp3 = new KelvinTemperature(0);
-        ICelcius KCA = new KCAdapter(temp3);
-        IFahrenheit KFA = new KFAdapter(temp3);
-
-        Thermometer T = new Thermometer();
-
-        T.showCelciusTemperature(temp1);
-        T.showKelvinTemperature(CKA);
-        T.showFahrenheitTemperature(CFA);
-
-        System.out.println();
-
-        T.showFahrenheitTemperature(temp2);
-        T.showCelciusTemperature(FCA);
-        T.showKelvinTemperature(FKA);
 
         System.out.println();
 

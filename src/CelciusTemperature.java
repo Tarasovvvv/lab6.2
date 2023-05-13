a8855e938f5c38179498ab4a4dec3c898ea8cfdb
Ivanov Sergey <96431033+qtenebrae@users.noreply.github.com>
2023-05-13 19:41:09 +0400

    Its broken
    fix it

--- src/CelciusTemperature.java
+++ src/CelciusTemperature.java
@@ -1,9 +1,3 @@
-public class CelciusTemperature implements ICelcius {
-    private double TEMPERATURE;
-
-    public CelciusTemperature(double temperature) {
-        this.TEMPERATURE = temperature;
-    }
 
     @Override
     public double getCelciusTemperature() {
